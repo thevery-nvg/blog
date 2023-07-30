df3151ae93cf07b08ddf2596e34cb0013eaf2138
box7 <theverynvg@vk.com>
2023-07-30 21:14:29 +0400

    pagination added

--- blog/templatetags/menu.py
+++ blog/templatetags/menu.py
@@ -4,7 +4,7 @@ from blog.models import Category
 register = template.Library()
 
 
-@register.inclusion_tag('blog/menu_tpl.html')
+@register.inclusion_tag('blog/../../templates/tpl/menu_tpl.html')
 def show_menu(menu_class='menu'):
     categories = Category.objects.all()
     return {'categories': categories, 'menu_class': menu_class}
